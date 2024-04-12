98b770f94da274d27fe148ad32ef32e720953a25
Yoann.Saget <yoasaget@gmail.com>
2024-04-12 11:56:01 +0200

    test

--- whatyearisit-frontend/script.js
+++ whatyearisit-frontend/script.js
@@ -1,8 +1,8 @@
 
 
-fetch('https://github.com/YoannS01/mynewapp.git)
+fetch('mynewapp-q69y90r9p-yoanns-projects-5da54912.vercel.app/year')
     .then(response => response.json())
     .then(data => {
-        document.querySelector('#icon').src = data.icon_url;
-        document.querySelector('#fact').textContent = data.value;
+        document.querySelector('#year').textContent = data.year;
     });
+
